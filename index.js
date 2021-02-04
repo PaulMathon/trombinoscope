d3280d586195c2d82cd7bd4d40a17e593d854845
PauloDeLaMontana <paul.mathon@outlook.fr>
2021-02-04 14:47:12 +0100

    Zoom v1.1

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
 import { fetchPractitioners, getCriterias } from "./js/data.js";
 import { newDisplay } from "./js/display.js";
 import { manageDisposition } from "./js/disposition.js";
+import { setZoomable } from "./js/zoom.js";
 
 function main() {
   fetchPractitioners()
     .then((data) => {
       const criterias = getCriterias(data);
       manageDisposition(data, criterias);
-      const displayContainer = newDisplay(data, criterias, ["specialities", "cabinets"]);
+      newDisplay(data, criterias, ["specialities", "cabinets"]);
+      setZoomable();
     });
-
-  const zoomableElements = document.getElementsByClassName("zoomable");
-  for (let i=0; i < zoomableElements.length; i++) {
-    const element = zoomableElements[i];
-    element.addEventListener("click", (event) => {
-      if (element.classList.contains("zoom")) {
-        element.parentElement.style.width = "100%";
-        element.parentElement.style.height = "100%";
-        element.parentElement.style.transform = "translate(0, 0)";
-        element.classList.remove("zoom");
-      } else {
-        element.parentElement.style.width = `${nbColumns*100}%`;
-        element.parentElement.style.height = `${nbLines*100}%`;
-        element.parentElement.style.transform =
-          `translate(-${element.offsetLeft*3}px, 0px)`;
-        element.classList.add("zoom");
-      }
-    });
-  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
