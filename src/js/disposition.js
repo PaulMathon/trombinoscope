9c229abf964fd5bc004ef42d3defe7f34281d437
PauloDeLaMontana <paul.mathon@outlook.fr>
2021-02-17 11:33:02 +0100

    Change disposition while zooming without context updated

--- src/js/disposition.js
+++ src/js/disposition.js
@@ -1,16 +1,18 @@
 import { createDisplay } from "./display.js";
+import {config} from "./config.js";
 
 export function manageDisposition(context, data, criterias) {
-  function onChangeDisposition(newDisposition) {
+  let currentDisposition=config.defaultPath;
+  function onChangeDisposition(criteria) {
     return (event) => {
-      
       switchActiveButton(event.target);
+      currentDisposition = getDisposition(context.path, currentDisposition, criteria);
 
       const displayContainer = document.getElementById("display-content");
       while (displayContainer.firstChild) {
         displayContainer.removeChild(displayContainer.firstChild);
       }
-      const newCurrentWindow = createDisplay(data, criterias, newDisposition);
+      const newCurrentWindow = createDisplay(data, criterias, currentDisposition);
       context.reset(newCurrentWindow);
     };
   }
@@ -18,15 +20,18 @@ export function manageDisposition(context, data, criterias) {
   const specialityButton = document.getElementById("speciality-btn");
   specialityButton.classList.add("active");
 
+  document.getElementById("none-btn").addEventListener("click",
+    onChangeDisposition([])
+  );
   specialityButton.addEventListener("click",
-    onChangeDisposition(["specialities", "cabinets"])
+    onChangeDisposition("specialities")
   );
   specialityButton.classList.add("focus");
   document.getElementById("city-btn").addEventListener("click",
-    onChangeDisposition(["cities", "specialities"])
+    onChangeDisposition("cities")
   );
   document.getElementById("cabinet-btn").addEventListener("click",
-    onChangeDisposition(["cabinets", "specialities"])
+    onChangeDisposition("cabinets")
   );
 }
 
@@ -34,4 +39,13 @@ function switchActiveButton(activeButton) {
   const dispositionButtons = document.querySelectorAll("#disposition-container button");
   dispositionButtons.forEach((button) => button.classList.remove("active"));
   activeButton.classList.add("active");
+}
+
+function getDisposition(path, dispositionPath, criteria) {
+  const pastPath = dispositionPath.slice(0, path.length - 1);
+  let nextPath = pastPath.length >= 2 ? [] : [criteria];
+  if (pastPath.length === 0) {
+    nextPath.push(criteria === "specialities" ? "cabinets" : "specialities");
+  }
+  return pastPath.concat(nextPath);
 }
